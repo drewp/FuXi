6b437eeb2d6ae252442be0ddca579c6628b36692
Drew Perttula <drewp@bigasterisk.com>
2014-02-16 21:28:46 -0800

    test for the HashablePatternList concat bug

--- test/test_network_pattern_list.py
+++ test/test_network_pattern_list.py
@@ -4,22 +4,21 @@ logging.basicConfig(level=logging.DEBUG)
 
 from FuXi.Rete.Network import HashablePatternList
 from rdflib import URIRef, Literal
-# import rdflib.term
-
+import rdflib.term
 
 class TestHashablePatternList(unittest.TestCase):
     def setUp(self):
         super(TestHashablePatternList, self).setUp()
-
-        # self.oldWarning = rdflib.term._LOGGER.warning
-        # def stopOnWarning(msg):
-        #     raise ValueError("this warning was logged: %s" % msg)
-        # rdflib.term._LOGGER.warning = stopOnWarning
-
+        
+        self.oldWarning = rdflib.term._LOGGER.warning
+        def stopOnWarning(msg):
+            raise ValueError("this warning was logged: %s" % msg)
+        rdflib.term._LOGGER.warning = stopOnWarning
+        
     def tearDown(self):
         super(TestHashablePatternList, self).tearDown()
-        # rdflib.term._LOGGER.warning = self.oldWarning
-
+        rdflib.term._LOGGER.warning = self.oldWarning
+        
     def testCombineUriAndLiteral(self):
         # This is a simplified version of input that happens in real usage with a rule like this:
         # { ?c :p1 ?uri . } => { ?c :p2 (" " ?uri) . } .
