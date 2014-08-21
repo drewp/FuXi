22263b0751a29839013ce43646dd18302c7b9bb1
Drew Perttula <drewp@bigasterisk.com>
2014-08-21 01:52:09 -0700

    ReteToken.concatenateTerms was assuming all terms could be str()'d to ascii
    not true for Literal(u'\xe9'), for example

--- lib/Rete/AlphaNode.py
+++ lib/Rete/AlphaNode.py
@@ -114,7 +114,7 @@ class ReteToken:
 
     @memoize
     def concatenateTerms(terms):
-        return reduce(lambda x, y: str(x) + str(y), [term[VALUE] for term in terms])
+        return ''.join(unicode(term[VALUE]) for term in terms)
 
     def __eq__(self, other):
         return hash(self) == hash(other)
